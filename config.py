1a13fe3ba2dadc4d03e1cded3341be3c51f9eba0
leul1.id <leultsehaye123@gmail.com>
2025-03-24 21:45:52 +0300

    removed conn

--- config.py
+++ config.py
@@ -6,13 +6,3 @@ DB_NAME = "BCDB"
 DB_USER = "postgres"
 DB_PASSWORD = "1234"
 DB_PORT = "5432"
-
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT
-    )
-    return conn
